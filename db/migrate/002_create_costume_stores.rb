e715bfec7ff1f53361729c34baf05332b0c7ecb7
Greg Morgan <gregm9999@gmail.com>
2018-11-23 22:19:46 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CostumeStore
+  def change
+    create_talbe :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :number_of_costumes
+      t.string :number_of_employees
+      t.string :still_in_business
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
