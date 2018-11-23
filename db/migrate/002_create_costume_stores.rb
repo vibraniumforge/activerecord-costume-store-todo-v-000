96d08322d6e09203b2e3b1bcd688a0f67ed96d97
Greg Morgan <gregm9999@gmail.com>
2018-11-23 22:35:53 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 # Create your costume_stores migration here
-class CreateCostumeStores < ActiveRecord::Migration
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_talbe :costume_stores do |t|
       t.string :name
