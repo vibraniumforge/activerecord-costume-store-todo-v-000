1e009e0cd89b4139bb77a6a36387f1538844596b
Greg Morgan <gregm9999@gmail.com>
2018-11-23 22:21:23 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class HauntedHouse
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.string :price
+      t.string :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+      t.string :long_description
+  end
+end
